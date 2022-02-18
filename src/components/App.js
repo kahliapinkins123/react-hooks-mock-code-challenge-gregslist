976cdfb6694b54c8aa2ab396aa6ba696b00c5ec2
Kahlia Pinkins <kahliapinkins123@gmail.com>
2022-02-17 22:07:46 -0500

    commit tree 11a229627c2775366a06443c89e6f19600231f6b

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listingList, setListingList] = useState([]);
+  const [input, setInput] = useState('');
+
+  const shownListings = listingList.filter((listing)=>{
+    return listing.description.toLowerCase().includes(input.toLowerCase());
+  })
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header input={input} setInput={setInput}/>
+      <ListingsContainer listingList={shownListings} setListingList={setListingList}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,33 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+
+function ListingsContainer({ listingList, setListingList}) {
+  
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((resp) => resp.json())
+      .then((listings) => {setListingList(listings)});
+  }, [setListingList]);
+
+  function handleDeleteListing(deletedListing){
+    const updatedListings = listingList.filter((listing)=>{
+      return listing.id !== deletedListing.id;
+    })
+    setListingList(updatedListings);
+  }
+
+  const listings = listingList.map((listing)=>{
+    return <ListingCard key={listing.id} listing={listing} onDelete={handleDeleteListing}/>
+  })
+
+
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings}
       </ul>
     </main>
   );
